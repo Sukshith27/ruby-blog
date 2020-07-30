d81d4c473701412e135fcabd0bdf8ffab435fd97
Sukshith <sukshith@habitnu.com>
2020-07-30 19:35:59 +0530

    show

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -13,4 +13,8 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     def article_params
       params.require(:article).permit(:title, :text)
     end
+    
+    def show
+      @article = Article.find(params[:id])
+    end
 end
